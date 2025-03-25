457525e984dc891d522004b35a5754c1eb61916a
Shubham <Shubhamsep23@gmail.com>
2025-03-26 00:01:18 +0530

    API Automation Framework 25th March 2025

--- src/test/java/Automation/org/Test/E2E/TestE2EScenario4.java
+++ src/test/java/Automation/org/Test/E2E/TestE2EScenario4.java
@@ -9,29 +9,19 @@ import io.qameta.allure.Owner;
 import io.restassured.RestAssured;
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
-import static org.assertj.core.api.Assertions.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
-
-public class TestE2EScenario1 extends BaseTest {
-    // Create a Booking--->> Booking ID
-    // Create Token
-    // Get the BookingID
-    // Update the Booking
-    // Delete the Booking
+public class TestE2EScenario4 extends BaseTest {
 
     @Description("TC-1 Create Booking")
     @Owner("Shubham")
     @Test(groups = "qa",priority = 1)
     public void createBooking(ITestContext iTestContext){
-        // Pre-requisites
-        requestSpecification.basePath(APIConstants.CREATE_READ_UPDATE_DELETE);
 
-        // Making HTTP request
+        requestSpecification.basePath(APIConstants.CREATE_READ_UPDATE_DELETE);
         response = RestAssured.given(requestSpecification).when()
                 .body(payloadManager.createPayloadBookingAsString()).post();
-
-        // Validating Response
         validatableResponse = response.then().log().all();
         validatableResponse.statusCode(200);
 
@@ -39,34 +29,15 @@ public class TestE2EScenario1 extends BaseTest {
         iTestContext.setAttribute("bookingid",bookingResponse.getBookingid());
     }
 
-    @Description("TC-2 Verify Booking ID")
+    @Description("Update Booking")
     @Owner("Shubham")
     @Test(groups = "qa",priority = 2)
-    public void verifyBookingID(ITestContext iTestContext){
-
-       Integer bookingid=(Integer) iTestContext.getAttribute("bookingid");
-
-       requestSpecification.basePath(APIConstants.CREATE_READ_UPDATE_DELETE+"/"+bookingid);
-       response=RestAssured.given(requestSpecification).get();
-       validatableResponse=response.then().log().all();
-       validatableResponse.statusCode(200);
-
-        Booking booking= payloadManager.getJavaObjectBooking(response.asString());
-        assertThat(booking.getFirstname()).isNotBlank().isNotNull();
-        assertThat(booking.getFirstname()).isEqualTo("Shubham");
-        assertThat(booking.getLastname()).isEqualTo("Sharma");
-    }
-
-    @Description("TC-3 Update Booking ID")
-    @Owner("Shubham")
-    @Test(groups = "qa",priority = 3)
     public void updateBookingID(ITestContext iTestContext){
-        Integer bookingid=(Integer) iTestContext.getAttribute("bookingid");
 
+        Integer bookingid=(Integer) iTestContext.getAttribute("bookingid");
         String token=getToken();
         iTestContext.setAttribute("token",token);
 
-        System.out.println(bookingid);
         requestSpecification.basePath(APIConstants.CREATE_READ_UPDATE_DELETE+"/"+bookingid);
         response=RestAssured.given(requestSpecification)
                 .cookie("token",token).when()
@@ -75,26 +46,27 @@ public class TestE2EScenario1 extends BaseTest {
         validatableResponse.statusCode(200);
 
         Booking booking =payloadManager.getJavaObjectBooking(response.asString());
-
         assertThat(booking.getTotalprice()).isPositive().isEqualTo(2000);
         assertThat(booking.getFirstname()).isNotBlank().isEqualToIgnoringCase("abc");
         assertThat(booking.getLastname()).isNotBlank().isEqualToIgnoringCase("xyz");
-
     }
 
-    @Description("TC-4 Delete Booking")
-    @Owner("Shubham")
-    @Test(groups = "qa",priority = 4)
-    public void deleteBooking(ITestContext iTestContext){
-        Integer bookingid= (Integer) iTestContext.getAttribute("bookingid");
-        String token= (String) iTestContext.getAttribute("token");
+    @Description("Delete Booking")
+    @Owner("Shubham SDET")
+    @Test(groups = "qa", priority = 3)
+    public void deleteBooking(ITestContext iTestContext) {
 
-        requestSpecification.basePath(APIConstants.CREATE_READ_UPDATE_DELETE+"/"+bookingid);
-        response=RestAssured.given(requestSpecification)
-                .cookie("token",token)
+        String token=(String) iTestContext.getAttribute("token");
+        Integer bookingid = (Integer) iTestContext.getAttribute("bookingid");
+
+        requestSpecification.basePath(APIConstants.CREATE_READ_UPDATE_DELETE + "/" + bookingid);
+        response = RestAssured.given(requestSpecification)
+                .cookie("token", token)
                 .when().delete();
-        validatableResponse=response.then().log().all();
+        validatableResponse = response.then().log().all();
         validatableResponse.statusCode(201);
     }
+
+
 
 }
